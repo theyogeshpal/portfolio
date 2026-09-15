1e1b07af9e396afda42047429541c603f154cd3e
Yogesh Pal <159111601+theyogeshpal@users.noreply.github.com>
2026-09-15 23:14:16 +0530

    Add TechStack component to HomePage

--- portfolio-react/src/pages/HomePage.jsx
+++ portfolio-react/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Home from '../components/Home';
 import About from '../components/About';
 import Education from '../components/Education';
 import Experience from '../components/Experience';
+import TechStack from '../components/TechStack';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 
@@ -13,6 +14,7 @@ export default function HomePage() {
       <About />
       <Education />
       <Experience />
+      <TechStack />
       <Projects />
       <Contact />
     </>
